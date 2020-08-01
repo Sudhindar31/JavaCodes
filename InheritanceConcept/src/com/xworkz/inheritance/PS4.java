69c345084a8458f4fcf711eb551f926c9495f1c0
Sudhindar31 <sudhindarkm@yahoo.com>
2020-08-01 22:01:31 +0530

    Encapsulation and Inheritance

---
+++ InheritanceConcept/src/com/xworkz/inheritance/PS4.java
@@ -0,0 +1,13 @@
+package com.xworkz.inheritance;
+
+public class PS4 extends Gaming {
+	
+	String accessories;
+	
+	void console(){
+		System.out.println("Accessories with the PS4 console : " + accessories);
+	}
+	
+	}
+
+
