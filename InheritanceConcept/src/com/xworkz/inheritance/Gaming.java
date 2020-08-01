436b3e6ac0a5a94bc6d82db60f8e28aed8d78364
Sudhindar31 <sudhindarkm@yahoo.com>
2020-08-01 22:01:31 +0530

    Encapsulation and Inheritance

---
+++ InheritanceConcept/src/com/xworkz/inheritance/Gaming.java
@@ -0,0 +1,14 @@
+package com.xworkz.inheritance;
+
+public class Gaming {
+
+	String gametype;
+	String platform; 
+	
+	void resolution(){
+		System.out.println("4K compatible");
+	}
+	void perspective(){
+		System.out.println("Perspective : " + gametype);
+	}
+}
---
+++ InheritanceConcept/src/com/xworkz/inheritance/GamingStudio.java
@@ -0,0 +1,15 @@
+package com.xworkz.inheritance;
+
+public class GamingStudio {
+
+	public static void main(String[] args) {
+		PS4 nd = new PS4();
+		nd.accessories = "VR";
+		nd.gametype = "TPP";
+		nd.resolution();
+		nd.perspective();
+		nd.console();
+
+	}
+
+}
